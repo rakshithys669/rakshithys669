867c9f27964c7e969c8b1f32f18bd3980d87a954
User <User@DESKTOP-AM3ERC0>
2022-11-30 19:15:30 +0530

    Build_002

--- Test_1/src/test/java/Demo.java
+++ Test_1/src/test/java/Demo.java
@@ -1,4 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo 
 
@@ -7,6 +11,9 @@ public class Demo
 
 public void test()
 {
+	WebDriverManager.chromedriver().setup();
+	
+	WebDriver driver = new ChromeDriver();
 	
 	
 }
